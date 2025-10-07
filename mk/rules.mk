457e7fe085a54fac5a4a5b69f25d034294cf6277
Beerus <brucejuniorallain@gmail.com>
2025-10-07 11:16:39 +0300

    Auto-commit on mar. 07 oct. 2025 11:16:39 EAT

--- mk/rules.mk
+++ mk/rules.mk
@@ -1,61 +1,64 @@
-output		:
-				@if [ ! -d $@ ]; then mkdir $@; fi
+output				:
+						@if [ ! -d $@ ]; then mkdir $@; fi
 
-output/%.o	: %.cpp | output
-				@$(call create_object, $<, $@)
+output/%.o			: %.cpp | output
+						@$(call create_object, $<, $@)
 
-run			: $(NAME)
-				./$(NAME) $(ARGS)
+run					: $(NAME)
+						./$(NAME) $(ARGS)
 
-run\:./%	: $(OBJS)
-				$(call runner,$(subst run:,,$@))
+run\:./%			: $(OBJS)
+						$(call runner,$(subst run:,,$@))
 
-rerun		: re run
-rerun\:./%	: re
-				$(call runner,$(subst rerun:,,$@))
+rerun				: re run
+rerun\:./%			: re
+						$(call runner,$(subst rerun:,,$@))
 
-revrun		: re vrun
-revrun\:./%	: re
-				$(call runner,$(subst revrun:,,$@), valgrind $(VALGRINDFLAGS))
+revrun				: re vrun
+revrun\:./%			: re
+						$(call runner,$(subst revrun:,,$@), valgrind $(VALGRINDFLAGS))
 
-revsrun		: re vsrun
-revsrun\:./%: re
-				$(call runner,$(subst revsrun:,,$@), valgrind $(SVALGRINDFLAGS))
+revsrun				: re vsrun
+revsrun\:./%		: re
+						$(call runner,$(subst revsrun:,,$@), valgrind $(SVALGRINDFLAGS))
 
-vrun		: $($NAME)
-				valgrind $(VALGRINDFLAGS) ./$(NAME) $(ARGS)
+vrun				: $($NAME)
+						valgrind $(VALGRINDFLAGS) ./$(NAME) $(ARGS)
 
-vrun\:./%	: $(OBJS)
-				$(call runner, $(subst vrun:,,$@), valgrind $(VALGRINDFLAGS))
+vrun\:./%			: $(OBJS)
+						$(call runner, $(subst vrun:,,$@), valgrind $(VALGRINDFLAGS))
 
-vsrun		: $(NAME)
-				valgrind $(SVALGRINDFLAGS) ./$(NAME) $(ARGS)
+vsrun				: $(NAME)
+						valgrind $(SVALGRINDFLAGS) ./$(NAME) $(ARGS)
 
-vsrun\:./%	: $(OBJS)
-				$(call runner,$(subst vsrun:,,$@), valgrind $(SVALGRINDFLAGS))
+vsrun\:./%			: $(OBJS)
+						$(call runner,$(subst vsrun:,,$@), valgrind $(SVALGRINDFLAGS))
 
-test		: $(NAME)
-				$(call loop_run,$(TEST_FILES))
-				$(call showNotTestedFiles,$(FILES_NOT_TESTED))
+test				: $(NAME)
+						$(call loop_run,$(TEST_FILES))
+						$(call showNotTestedFiles,$(FILES_NOT_TESTED))
 
-test\:%		: $(OBJS)
-				$(call loop_run, $(filter ./.tests/$(subst test:,,$@)/%, $(TEST_FILES)))
+test\:%				: $(OBJS)
+						$(call loop_run, $(filter ./.tests/$(subst test:,,$@)/%, $(TEST_FILES)))
 
-commit		:
-				@$(call create_commit)
+commit				:
+						@$(call create_commit)
 
-commit\:%	: 
-				@$(call create_commit,$(subst commit:,,$@))
+commit\:%			: 
+						@$(call create_commit,$(subst commit:,,$@))
 
-push		: commit
-				@git push
+push				: commit
+						@git push
 
-push\:%		:
-				@$(call create_commit,$(subst push:,,$@))
-				@git push
+push\:%				:
+						@$(call create_commit,$(subst push:,,$@))
+						@git push
 
-lib			: $(OBJS)
-				ar rcs $(NAME).a $(OBJS)
+lib					: $(OBJS)
+						@ar rcs $(NAME).a $(OBJS)
 
-create_dep_lib	:
-					$(call make_loop_libs, lib)
+create_dep_lib		:
+						$(call make_loop_libs, lib)
+
+status				:
+						@git status
